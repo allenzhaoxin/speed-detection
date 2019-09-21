28305318d22c416a7f9769f68078a50446cfad0e
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2019-09-21 10:06:27 -0300

    :building_construction: Organiza amostra de videos

--- main.py
+++ main.py
@@ -2,9 +2,22 @@ import cv2
 
 from VideoInput import VideoInput
 
-video = VideoInput('example1.mp4')
+video1 = VideoInput('video_samples/sample_1.mp4')
+video2 = VideoInput('video_samples/sample_2.mp4')
 
-print('Width: ', video.width)
-print('Height: ', video.height)
+# Play video 1
 
-video.show_video()
+print('Video Sample 1')
+print('Width: ', video1.width)
+print('Height: ', video1.height)
+
+video1.show_video()
+
+# Play video 1
+
+print('Video Sample 2')
+print('Width: ', video2.width)
+print('Height: ', video2.height)
+
+video2.show_video()
+
