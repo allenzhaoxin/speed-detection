ab55c534f22e0290bf96d9013641db937f409d2a
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2019-09-21 09:49:32 -0300

    :rocket: Adiciona VideoInput
    VideoInput é uma classe para ajudar a gerenciar os videos, assim facilitando o acesso a tais informações.
    Além disso, foi adicionado um examplo de vídeo para testes (example1.mp4).

---
+++ VideoInput.py
@@ -0,0 +1,50 @@
+import cv2
+
+
+class VideoInput:
+
+    _path = None
+    width = None
+    height = None
+    video = None
+
+    def __init__(self, path):
+        self._path = path
+        self.load_video()
+
+    def load_video(self):
+        self.video = cv2.VideoCapture(self._path)
+        self.capture_dimension()
+
+    def capture_dimension(self):
+        if self.video.isOpened():
+            self.width = self.video.get(3)
+            self.height = self.video.get(4)
+        else:
+            print("Error opening video stream or file")
+
+    def show_video(self):
+        if self.video.isOpened():
+            while self.video.isOpened():
+                # Capture frame-by-frame
+                ret, frame = self.video.read()
+                if ret == True:
+
+                    # Display the resulting frame
+                    cv2.imshow('Video: ' + str(self._path) + ' (press Q to EXIT)', frame)
+
+                    # Press Q on keyboard to  exit
+                    if cv2.waitKey(25) & 0xFF == ord('q'):
+                        break
+
+                # Break the loop
+                else:
+                    break
+        else:
+            print("Error opening video stream or file")
+
+        # When everything done, release the video capture object
+        self.video.release()
+
+        # Closes all the frames
+        cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-// Main file
+import cv2
+
+from VideoInput import VideoInput
+
+video = VideoInput('example1.mp4')
+
+print('Width: ', video.width)
+print('Height: ', video.height)
+
+video.show_video()
