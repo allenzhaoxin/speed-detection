3ab00933acf9ed0a170550dc3eb70f06ab02ccb2
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2019-09-21 10:27:25 -0300

    :hammer: Adiciona novas amostras de vídeos

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from VideoInput import VideoInput
 
 video1 = VideoInput('video_samples/sample_1.mp4')
 video2 = VideoInput('video_samples/sample_2.mp4')
+video3 = VideoInput('video_samples/sample_3.mp4')
+video4 = VideoInput('video_samples/sample_4.avi')
 
 # Play video 1
 
@@ -13,7 +15,7 @@ print('Height: ', video1.height)
 
 video1.show_video()
 
-# Play video 1
+# Play video 2
 
 print('Video Sample 2')
 print('Width: ', video2.width)
@@ -21,3 +23,18 @@ print('Height: ', video2.height)
 
 video2.show_video()
 
+# Play video 3
+
+print('Video Sample 3')
+print('Width: ', video3.width)
+print('Height: ', video3.height)
+
+video3.show_video()
+
+# Play video 4
+
+print('Video Sample 4')
+print('Width: ', video4.width)
+print('Height: ', video4.height)
+
+video4.show_video()
