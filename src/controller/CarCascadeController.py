c378688942e32fde7fdc29e975f0243dfc5004f1
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2019-10-26 10:57:50 -0300

    :hammer: Automatizado inserção do PATH

--- src/controller/CarCascadeController.py
+++ src/controller/CarCascadeController.py
@@ -1,8 +1,7 @@
+import os.path
 import cv2
 
 from src.controller.PreProcessController import pre_process_frame
-
-path = '/home/gabriel-mesquita/Área de Trabalho/speed-detection/speed-detection/resource''/cascades/car.xml'
 
 
 class CarCascadeController:
@@ -10,7 +9,10 @@ class CarCascadeController:
     classifier = None
 
     def __init__(self):
-        self.classifier = cv2.CascadeClassifier(path)
+        ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+        CASCADE_PATH = os.path.join(ROOT_DIR, '..', '..', 'resource', 'cascades', 'car.xml')
+
+        self.classifier = cv2.CascadeClassifier(CASCADE_PATH)
 
     def detect_cars(self, frame_object):
         return self.classifier.detectMultiScale(pre_process_frame(frame_object.image), 1.1, 13, 18, (24, 24))
