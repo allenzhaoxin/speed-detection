f4e5d26b04e99ec317ba6e82930e5afe83d01948
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2019-11-10 10:33:42 -0300

    :construction: Move video_samples para a pasta input

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from src.view.VideoViewer import VideoViewer
 
-video = VideoViewer('resource/video_samples/teste_controlado_100_lado1.mp4', 17)
+video = VideoViewer('resource/video_samples/input/teste_controlado_100_lado1.mp4', 17)
 video.run(VideoViewer.MODE_OUTPUT)
