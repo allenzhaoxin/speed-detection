832324c3ee5de473a95610e25bbd5a406cbb9cd6
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2019-11-13 14:22:06 -0300

    :bug: Corrige outputs que foram gerados com a taxa FPS errada

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
 from src.view.VideoViewer import VideoViewer
 
-video1 = VideoViewer('resource/video_samples/input/teste_controlado_60_lado1.mp4', 19)
-video1.run(VideoViewer.MODE_OUTPUT)
+# ---------------------------------------------------------- #
+#                     SPEED DETECTION                        #
+#             Author: Gabriel Teixeira Mesquita              #
+# ---------------------------------------------------------- #
+#                     VIDEO VIEWER MODES:                    #
+#                                                            #
+#  1 - MODE_RUN_AND_PLAY -> SHOW THE VIDEO WITCH EACH FRAME  #
+#  2 - MODE_PRE_COMPILE  -> PROCESS ALL FRAME AFTER SHOW     #
+#  3 - MODE_OUTPUT       -> GENERATES A OUTPUT VIDEO FILE    #
+# ---------------------------------------------------------- #
+# ---------------------------------------------------------- #
 
-# video2 = VideoViewer('resource/video_samples/input/teste_controlado_60_lado2.mp4', 7)
-# video2.run(VideoViewer.MODE_OUTPUT)
 
-# video3 = VideoViewer('resource/video_samples/input/teste_controlado_80_lado1.mp4', 19)
-# video3.run(VideoViewer.MODE_OUTPUT)
+# VIDEO SAMPLES - DAY 1
 
-# video4 = VideoViewer('resource/video_samples/input/teste_controlado_80_lado2.mp4', 7)
-# video4.run(VideoViewer.MODE_OUTPUT)
+video = VideoViewer('resource/video_samples/input/teste_controlado_60_day1.mp4', 19)
+video.run(VideoViewer.MODE_RUN_AND_PLAY)
 
-# video5 = VideoViewer('resource/video_samples/input/teste_controlado_100_lado1.mp4', 17)
-# video5.run(VideoViewer.MODE_OUTPUT)
+# video = VideoViewer('resource/video_samples/input/teste_controlado_80_day1.mp4', 19)
+# video.run(VideoViewer.MODE_OUTPUT)
 
-# video6 = VideoViewer('resource/video_samples/input/teste_controlado_100_lado2.mp4', 7)
-# video6.run(VideoViewer.MODE_OUTPUT)
+# video = VideoViewer('resource/video_samples/input/teste_controlado_100_day1.mp4', 17)
+# video.run(VideoViewer.MODE_OUTPUT)
+
+# VIDEO SAMPLES - DAY 2
+
+# video = VideoViewer('resource/video_samples/input/teste_controlado_50_day2.mp4', 1)
+# video.run(VideoViewer.MODE_OUTPUT)
+
+# video = VideoViewer('resource/video_samples/input/teste_controlado_60_day2.mp4', 30)
+# video.run(VideoViewer.MODE_OUTPUT)
+
+# video = VideoViewer('resource/video_samples/input/teste_controlado_80_day2.mp4', 27)
+# video.run(VideoViewer.MODE_OUTPUT)
+
+# video = VideoViewer('resource/video_samples/input/teste_controlado_100_day2.mp4', 33)
+# video.run(VideoViewer.MODE_OUTPUT)
