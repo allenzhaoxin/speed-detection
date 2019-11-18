2ba48048d85c4a0b21540429c696baf6c8bf05b3
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2019-11-18 01:10:28 -0300

    :hammer: Adiciona vídeos para o teste 3

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ from src.view.VideoViewer import VideoViewer
 # ---------------------------------------------------------- #
 
 
-# VIDEO SAMPLES - DAY 1
+# VIDEO SAMPLES - DAY 1 - SIDE 1
 
-video = VideoViewer('resource/video_samples/input/teste_controlado_60_day1.mp4', 19)
-video.run(VideoViewer.MODE_RUN_AND_PLAY)
+# video = VideoViewer('resource/video_samples/input/teste_controlado_60_day1.mp4', 19)
+# video.run(VideoViewer.MODE_RUN_AND_PLAY)
 
 # video = VideoViewer('resource/video_samples/input/teste_controlado_80_day1.mp4', 19)
 # video.run(VideoViewer.MODE_OUTPUT)
@@ -24,10 +24,7 @@ video.run(VideoViewer.MODE_RUN_AND_PLAY)
 # video = VideoViewer('resource/video_samples/input/teste_controlado_100_day1.mp4', 17)
 # video.run(VideoViewer.MODE_OUTPUT)
 
-# VIDEO SAMPLES - DAY 2
-
-# video = VideoViewer('resource/video_samples/input/teste_controlado_50_day2.mp4', 1)
-# video.run(VideoViewer.MODE_OUTPUT)
+# VIDEO SAMPLES - DAY 2 - SIDE 1
 
 # video = VideoViewer('resource/video_samples/input/teste_controlado_60_day2.mp4', 30)
 # video.run(VideoViewer.MODE_OUTPUT)
@@ -36,4 +33,62 @@ video.run(VideoViewer.MODE_RUN_AND_PLAY)
 # video.run(VideoViewer.MODE_OUTPUT)
 
 # video = VideoViewer('resource/video_samples/input/teste_controlado_100_day2.mp4', 33)
+# video.run(VideoViewer.MODE_OUTPUT)
+
+# VIDEO SAMPLES - DAY 3 - SIDE 1
+
+# video = VideoViewer('resource/video_samples/input/teste_controlado_60_day3_1.mp4', 28)
+# video.run(VideoViewer.MODE_OUTPUT)
+
+# video = VideoViewer('resource/video_samples/input/teste_controlado_60_day3_2.mp4', 22)
+# video.run(VideoViewer.MODE_OUTPUT)
+
+# video = VideoViewer('resource/video_samples/input/teste_controlado_60_day3_3.mp4', 31)
+# video.run(VideoViewer.MODE_OUTPUT)
+
+# video = VideoViewer('resource/video_samples/input/teste_controlado_80_day3_1.mp4', 16)
+# video.run(VideoViewer.MODE_OUTPUT)
+
+# video = VideoViewer('resource/video_samples/input/teste_controlado_80_day3_2.mp4', 31)
+# video.run(VideoViewer.MODE_OUTPUT)
+
+# video = VideoViewer('resource/video_samples/input/teste_controlado_80_day3_3.mp4', 19)
+# video.run(VideoViewer.MODE_OUTPUT)
+
+# video = VideoViewer('resource/video_samples/input/teste_controlado_100_day3_1.mp4', 35)
+# video.run(VideoViewer.MODE_OUTPUT)
+
+# video = VideoViewer('resource/video_samples/input/teste_controlado_100_day3_2.mp4', 33)
+# video.run(VideoViewer.MODE_OUTPUT)
+
+# video = VideoViewer('resource/video_samples/input/teste_controlado_100_day3_3.mp4', 31)
+# video.run(VideoViewer.MODE_OUTPUT)
+
+# VIDEO SAMPLES - DAY 3 - SIDE 2
+
+# video = VideoViewer('resource/video_samples/input/teste_controlado_60_day3_1 (lado2).mp4', 29)
+# video.run(VideoViewer.MODE_OUTPUT)
+
+# video = VideoViewer('resource/video_samples/input/teste_controlado_60_day3_2 (lado2).mp4', 28)
+# video.run(VideoViewer.MODE_OUTPUT)
+
+# video = VideoViewer('resource/video_samples/input/teste_controlado_60_day3_3 (lado2).mp4', 25)
+# video.run(VideoViewer.MODE_OUTPUT)
+
+# video = VideoViewer('resource/video_samples/input/teste_controlado_80_day3_1 (lado2).mp4', 22)
+# video.run(VideoViewer.MODE_OUTPUT)
+
+# video = VideoViewer('resource/video_samples/input/teste_controlado_80_day3_2 (lado2).mp4', 23)
+# video.run(VideoViewer.MODE_OUTPUT)
+
+# video = VideoViewer('resource/video_samples/input/teste_controlado_80_day3_3 (lado2).mp4', 27)
+# video.run(VideoViewer.MODE_OUTPUT)
+
+# video = VideoViewer('resource/video_samples/input/teste_controlado_100_day3_1 (lado2).mp4', 26)
+# video.run(VideoViewer.MODE_OUTPUT)
+
+# video = VideoViewer('resource/video_samples/input/teste_controlado_100_day3_2 (lado2).mp4', 24)
+# video.run(VideoViewer.MODE_OUTPUT)
+
+# video = VideoViewer('resource/video_samples/input/teste_controlado_100_day3_3 (lado2).mp4', 26)
 # video.run(VideoViewer.MODE_OUTPUT)
